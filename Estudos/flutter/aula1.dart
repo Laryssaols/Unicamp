faa9a1fd3288d4f7bea22da282970f053cadc20a
Laryssaols <l173501@dac.unicamp.br>
2024-02-22 15:36:08 -0300

    Add ao git

---
+++ Estudos/flutter/aula1.dart
@@ -0,0 +1,23 @@
+//variaveis 
+
+var firstvar; 
+
+
+//declaração de funções
+void main(){
+  firstvar = "Hello Word";
+  print(firstvar);
+	printName();
+}
+//funcoes assincronas - API, BD
+//funcoes sincrona wifi
+//funcoes recursivas - stack overflow
+//funcao callback
+printName(){
+	print("Hello Word");
+}
+void main(){
+  final duration;  
+}
+
+
